5b6e76f8db82e1cae7714a6ee460e55d3a423067
mafoloEmmanuel <mafoloemmanuel@gmail.com>
2021-07-20 13:14:49 +0200

    added node_modules to .gitignore

--- greet_xola.js
+++ greet_xola.js
@@ -5,7 +5,7 @@ const styledMessage = chalk.bgGreen.black(greet('Xola'))
 //console.log(styledMessage);
 
 const figlet = require('figlet');
-figlet(greet('Xola'), function(err, data) {
+figlet( styledMessage , function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
