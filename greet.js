e68442aceabf669dd3b10c838cfcb5577ad9f67a
mafoloEmmanuel <mafoloemmanuel@gmail.com>
2021-07-20 11:39:32 +0200

    initial commit

---
+++ greet.js
@@ -0,0 +1,10 @@
+/*function greet(name){
+    return ("Hello, " + name);
+  }
+  var msg = greet('Xola');
+  console.log(msg);*/
+
+  // create a module
+  module.exports = function(name){
+      return ("Hello, " + name);
+  }
---
+++ greet_xola.js
@@ -0,0 +1,15 @@
+//import the greet module that is in the current folder 
+const chalk = require('chalk');
+const greet = require('./greet');
+const styledMessage = chalk.bgGreen.black(greet('Xola'))
+//console.log(styledMessage);
+
+const figlet = require('figlet');
+figlet(greet('Xola'), function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
